96044acfed8ec52666f323cb9f92798c12623bc3
bheema-284 <bheemudug@gmail.com>
2022-04-11 23:19:22 +0530

    new project added

--- scriptAndPages/projects.js
+++ scriptAndPages/projects.js
@@ -1,5 +1,109 @@
 let projectsSection = document.getElementById('mainDivProjectsSection');
 projectsSection.innerHTML = `
+
+<!-- Cricbuzz project -->
+            <div class="eachProjectDiv">
+                <p class="ProjectName">Cricbuzz </p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/crichome.PNG" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>Cricbuzz is an Indian cricket news website owned by Times Internet. It features, news, articles and live coverage of cricket matches including videos, text commentary, player stats and team rankings.  </p>
+                        <p>This peoject is designed using HTML, CSS, JavaScript and Advance JavaScript</p>
+                        <br />
+                        <p> 
+                        </p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+
+                        <br>
+                        <div class="techStackProject">
+                            <p> <i class="fab fa-react"></i>React</p>
+                           <p> <svg
+    width="25"
+    height="20"
+    viewBox="0 0 256 208"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMinYMin meet">
+    <path
+      d="M15.624 35.012v87.53l15.058-8.471V43.482l-15.058-8.47z"
+      fill="#949494"
+    />
+    <path
+      d="M.565 114.07V8.66l89.411 50.823v17.883L15.624 35.012v87.53L.564 114.07z"
+      fill="#EFEFEF"
+    />
+    <path
+      d="M181.27 8.659V114.07l-60.235 33.882-15.059-9.412 60.236-33.882V34.07L89.976 77.365V59.482L181.271 8.66z"
+      fill="#949494"
+    />
+    <path
+      d="M166.212.188L90.918 42.541 15.624.188.564 8.658l89.412 50.824L181.271 8.66 166.21.189z"
+      fill="#D6D6D6"
+    />
+    <path
+      d="M151.153 42.541v53.647l15.059 8.47V34.072l-15.06 8.47z"
+      fill="#EFEFEF"
+    />
+    <path
+      d="M75.859 138.541l75.294-42.353 15.059 8.47-60.236 33.883 59.295 33.883 60.235-33.883 15.059 8.47-75.294 42.354-89.412-50.824z"
+      fill="#D6D6D6"
+    />
+    <path
+      d="M75.859 138.541v17.883l89.412 50.823v-17.882L75.859 138.54zM240.565 147.012V94.306l-15.06-8.47v52.705l15.06 8.47z"
+      fill="#EFEFEF"
+    />
+    <path
+      d="M165.27 189.365l75.295-42.353V94.306l15.059-8.47v70.588l-90.353 50.823v-17.882z"
+      fill="#949494"
+    />
+    <path
+      d="M255.624 85.835l-15.06 8.47-15.058-8.47 15.059-8.47 15.059 8.47z"
+      fill="#D6D6D6"
+    />
+    <path
+      d="M240.565 77.365V59.482l15.059-8.47v17.882l-15.06 8.47z"
+      fill="#949494"
+    />
+    <path
+      d="M240.565 59.482v17.883l-15.06-8.47V51.011l15.06 8.47z"
+      fill="#EFEFEF"
+    />
+    <path
+      d="M255.624 51.012l-15.06 8.47-15.058-8.47 15.059-8.47 15.059 8.47z"
+      fill="#D6D6D6"
+    />
+  </svg>
+  Material UI</p>
+                            <p> <i class="fab fa-js-square"></i> JavaScript </p>
+                            <p> <i class="fab fa-js-square"></i> ES6 </p>
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>                          
+           
+        <p><i class="fas fa-database"></i>
+        MongoDb</p>    
+        <p><i class="fab fa-node-js"></i>
+        ExpressJs</p>      
+       <p> <i class="fab fa-node-js"></i>
+        NodeJs</p>
+   
+                          
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/bheema-284/cricbuzz"
+                                target="blank"><button>
+                                    Github</button></a>
+                            <a href="https://newcricbuzzcb.netlify.app/" target="blank"><button>Demo</button></a>
+                           
+ <a href="https://medium.com/@bheemudug/cloning-cricbuzz-595b1b2e5694" target="blank"><button>Blog</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
 <!-- dunzo project -->
 <div class="eachProjectDiv dunzo">
                 <p class="ProjectName ">dunzo</p>
@@ -68,43 +172,7 @@ projectsSection.innerHTML = `
                     </div>
                 </div>
             </div>
-            
-            
-            <!-- Recipe Searching project -->
-            <div class="eachProjectDiv">
-                <p class="ProjectName">Recipe Searching Mini Project</p>
-                <div class="ProjectContainer">
-                    <div class="ProjectImage">
-                        <img src="./images/recipe1.PNG" alt="" />
-                    </div>
-                    <div class="projectDescription">
-                        <p>Recipe Searching Website was a mini project through this project I was learning making API call. </p>
-                        <p>This project is designed using HTML, CSS and JavaScript</p>
-                        <br />
-                        <p> 
-                        </p>
-                        <br>
-                        <u style="text-decoration: underline;">TechStack :- </u>
-                        <br>
-
-                        <br>
-                        <div class="techStackProject">
-                            <p> <i class="fab fa-js-square"></i> JavaScript </p>                            
-                            <p><i class="fab fa-html5"></i> HTML</p>
-                            <p><i class="fab fa-css3-alt"></i> CSS</p>
-                          
-                        </div>
-                        <div class="ProjectLinks">
-                            <a href="https://github.com/bheema-284/recipe_search"
-                                target="blank"><button>
-                                    Github</button></a>
-                            <a href="https://recipe-search-phi.vercel.app/" target="blank"><button>Demo</button></a>
-                           
-<a href="https://medium.com/@bheemudug/search-recipes-app-6d5407d192e8" target="blank"><button>Blog</button></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        
             
 
 <!-- GitHub Repo Searching project -->
